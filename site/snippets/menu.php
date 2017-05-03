ca01ddf063e262c8dfa4f0a83427ef464deffaf6
Richard McCartney <richard@mccartney.io>
2017-05-03 13:16:38 +0100

    sidebar navigation markup

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -7,30 +7,32 @@ $items = $pages->visible();
 if($items->count()):
 
 ?>
-<nav>
-  <ul>
-    <?php foreach($items as $item): ?>
-    <li>
-      <a<?php e($item->isOpen(), ' class="active"') ?> href="<?php echo $item->url() ?>"><?php echo $item->title()->html() ?></a>
+<nav class="sidebar__nav" role="tablist" aria-multiselectable="true">
+    <ul class="sidebar__nav-list">
+        <?php foreach($items as $item): ?>
+        <li class="sidebar__nav-group">
+            <button class="sidebar__nav-title" id="<?php echo $item->title()->html() ?>-tab" role="tab" aria-controls="<?php echo $item->title()->html() ?>"><?php echo $item->title()->html() ?></button>
+            <?php
 
-      <?php
+            // get all children for the current menu item
+            $children = $item->children()->visible();
 
-      // get all children for the current menu item
-      $children = $item->children()->visible();
+            // display the submenu if children are available
+            if($children->count() > 0):
 
-      // display the submenu if children are available
-      if($children->count() > 0):
-
-      ?>
-      <ul>
-        <?php foreach($children as $child): ?>
-        <li><a<?php e($child->isOpen(), ' class="active"') ?> href="<?php echo $child->url() ?>"><?php echo $child->title()->html() ?></a></li>
+            ?>
+            <section class="sidebar__nav-section" id="overview" role="tabpanel" aria-hidden="false" aria-labelledby="<?php echo $item->title()->html() ?>-tab">
+                <ul class="sidebar__nav-list">
+                    <?php foreach($children as $child): ?>
+                    <li class="sidebar__nav-item">
+                        <a<?php e($child->isOpen(), ' aria-current="page"') ?> href="<?php echo $child->url() ?>" class="sidebar__nav-link" ><?php echo $child->title()->html() ?></a>
+                    </li>
+                    <?php endforeach ?>
+                </ul>
+            </section>
+            <?php endif ?>
+        </li>
         <?php endforeach ?>
-      </ul>
-      <?php endif ?>
-
-    </li>
-    <?php endforeach ?>
-  </ul>
+    </ul>
 </nav>
 <?php endif ?>
