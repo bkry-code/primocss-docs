aa0fed1c5124dcf31527f5fcb0266bea40ee02aa
Richard McCartney <richard@mccartney.io>
2017-05-03 17:10:24 +0100

    moving the intro markup

--- site/templates/introduction.php
+++ site/templates/introduction.php
@@ -8,8 +8,6 @@
                         <h1><?= $page->title()->html() ?></h1>
                     </div>
                 </div>
-            </section>
-            <section class="strip">
                 <div class="grid-row grid-row--center">
                     <div class="grid-col-12 grid-md-col-10">
                         <?= $page->intro()->markdown() ?>
