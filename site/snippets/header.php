3a6262e9f6374a9860c1475657a12788457374b6
Richard McCartney <richard@mccartney.io>
2017-06-05 23:10:26 +0000

    Adding logo

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,7 +1,21 @@
 <div class="flex-container">
     <header class="sidebar flex-container__sidebar">
         <div class="sidebar__logo">
-            <h1><?= $site->title()->html() ?></h1>
+            <h1>
+			   <svg width="52px" height="52px" viewBox="0 0 170 170" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="float: left;">
+                            <!-- Generator: Sketch 42 (36781) - http://www.bohemiancoding.com/sketch -->
+                            <title>Group</title>
+                            <desc>Created with Sketch.</desc>
+                            <defs></defs>
+                            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g id="Group" transform="translate(0.000000, -3.000000)">
+                                    <rect id="Rectangle" fill="#ED4B37" x="0" y="3" width="170" height="170" rx="8"></rect>
+                                    <text id="P" font-family="HelveticaNeue-Bold, Helvetica Neue" font-size="144" font-weight="bold" letter-spacing="0.428571403" fill="#FFFFFF">
+                                        <tspan x="39.2617143" y="140">P</tspan>
+                                    </text>
+                                </g>
+                            </g>
+                        </svg>&nbsp;<?= $site->title()->html() ?></h1>
         </div>
         <?php snippet('menu') ?>
     </header>
