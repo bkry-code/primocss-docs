74c0775fadf750a6fcdcd2b6d301a532cc8a16bc
Richard McCartney <richard@mccartney.io>
2017-06-07 09:38:48 +0100

    Adding resizing to the logo and amending the footer styles for mobile views

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,11 +1,11 @@
-            <footer class="strip">
+            <footer class="strip footer">
                 <div class="grid-row grid-row--center">
                     <div class="grid-col-12 grid-md-col-11">
                         <div class="grid-row">
-                            <div class="grid-col-12 grid-md-col-6">
+                            <div class="grid-col-12 grid-md-col-6 footer__text">
                                 <?= $site->copyright()->markdown() ?>
                             </div>
-                            <div class="grid-col-12 grid-md-col-6">
+                            <div class="grid-col-12 grid-md-col-6 footer__text">
                                 <?= $site->creation()->markdown() ?>
                             </div>
                         </div>
